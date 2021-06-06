23025bb788098179f272316a70d49b3655546829
Your Name <gabryel.ehs@gmail.com>
2021-06-06 18:29:50 -0300

    details

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -63,7 +63,7 @@ class Detalhes extends StatelessWidget {
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(top: 20),
                   child: Text(
                     "Duração: $duracao",
                     style: TextStyle(
@@ -75,31 +75,109 @@ class Detalhes extends StatelessWidget {
             ),
             Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: Text(
-                    "Valor: $valor",
-                    style: TextStyle(
-                    fontSize: 18,
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Text(
+                        "Valor: $valor",
+                        style: TextStyle(
+                        fontSize: 18,
+                        ),
+                      ),
                     ),
-                  ),
-                )
+                  ],
+                ),
+                Column(
+                  children: [
+                     Padding(
+                      padding: const EdgeInsets.only(top: 15,left: 100),
+                      child:  GestureDetector(
+                           onTap: () =>  (
+                            ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                              "Link para finalizar inscrição enviado",
+                                textAlign: TextAlign.center,
+                              ),
+                                  duration: const Duration(milliseconds: 3000),
+                                  backgroundColor: Colors.grey.shade700,
+                                  behavior: SnackBarBehavior.floating,
+                                  shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30))),
+                                  width: 200,
+                              ),
+                            )
+
+                        ),
+                      
+                      child: Container(
+
+                          width: 100,
+                          height: 40,
+                           decoration: BoxDecoration( 
+                                  gradient: LinearGradient(
+                                  colors: [const Color(0xFFFFFFFFF), const Color(0xFFa3ddff)],
+                                  begin: const FractionalOffset(0.0, 0.0),
+                                  end: const FractionalOffset(3.5, 0.0),
+                                  stops: [0.0, 0.5],
+                                  tileMode: TileMode.clamp),
+                                  border: Border.all(
+                                      width: 2.5,
+                                      color: Colors.lightBlue
+                                ),
+                            borderRadius: BorderRadius.circular(50)
+                           ), 
+                        child :Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                          "Inscrição",
+                          style: TextStyle(
+                          fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                      ),
+                        ),
+                      )
+                      )
+                    ),
+                  ],
+                ),
               ],
             ),
+          ],
+          
+        ),
+      ),
+    );
+
+
+     final container = Material(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            descricao,
+            detalhes,
+
           ],
         ),
       ),
     );
 
+
     return Scaffold(
-      body: Column(
+      body: SingleChildScrollView(
+      child: Column(
         children: <Widget>[
           new GradientAppBaar(""),
           topContent,
-          descricao,
-          detalhes,
+          container,
         ],
       ),
+      )
     );
   }
 }
